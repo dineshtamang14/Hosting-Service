d6e632624e0dd87b1aeee41eca9b554e1925801a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-09-30 16:21:11 +0530

    add the mobile navbar

--- shared.js
+++ shared.js
@@ -1,18 +1,37 @@
 let dropback = document.querySelector(".dropback");
 let modal = document.querySelector(".modal");
 let buttons = document.querySelectorAll(".plan .button");
+let toggle = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
 // to display the modal and dropback
 for(let i=0; i<buttons.length; i++){
     buttons[i].addEventListener("click", function(){
-        modal.style.display = "block";
-        dropback.style.display = "block";
+        modal.classList.add("open");
+        dropback.classList.add("open");
     });
 }
 
 // to hide the modal and dropback
 let modalButton = document.querySelector(".btn");
-modalButton.addEventListener("click", function(){
-    modal.style.display = "none";
-    dropback.style.display = "none";
+
+if(modalButton){
+    modalButton.addEventListener("click", closeModal);
+}
+
+dropback.addEventListener("click", function(){
+    mobileNav.classList.remove("open");
+    closeModal();
+});
+
+function closeModal(){
+    if(modal){
+        modal.classList.remove("open");
+    }
+    dropback.classList.remove("open");
+}
+
+toggle.addEventListener("click", function(){
+    mobileNav.classList.add("open");
+    dropback.classList.add("open");
 });
