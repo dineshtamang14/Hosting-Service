cd1efad2c7ab840a803a1ab0bfea792da6a45a3a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-25 11:43:45 +0530

    added flexbox

--- shared.js
+++ shared.js
@@ -1,37 +1,45 @@
-let dropback = document.querySelector(".dropback");
-let modal = document.querySelector(".modal");
-let buttons = document.querySelectorAll(".plan .button");
-let toggle = document.querySelector(".toggle-button");
-let mobileNav = document.querySelector(".mobile-nav");
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
-// to display the modal and dropback
-for(let i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click", function(){
-        modal.classList.add("open");
-        dropback.classList.add("open");
-    });
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
 }
 
-// to hide the modal and dropback
-let modalButton = document.querySelector(".btn");
-
-if(modalButton){
-    modalButton.addEventListener("click", closeModal);
-}
-
-dropback.addEventListener("click", function(){
-    mobileNav.classList.remove("open");
-    closeModal();
+backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
 });
 
-function closeModal(){
-    if(modal){
-        modal.classList.remove("open");
-    }
-    dropback.classList.remove("open");
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
 }
 
-toggle.addEventListener("click", function(){
-    mobileNav.classList.add("open");
-    dropback.classList.add("open");
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
